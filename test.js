4fe0148c353fbfa1fb1b32e0fa3dc22b853d2864
Alooking <127853240+Alooking@users.noreply.github.com>
2024-08-26 22:42:56 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,11 @@ const puppeteer = require('puppeteer');
         executablePath: '/opt/render/project/.render/chrome/opt/google/chrome/google-chrome',
         args: ['--no-sandbox']
     });
+console.log("browser launched");
+
     const page = await browser.newPage();
-    await page.goto('https://example.com');
+    await page.goto('https://google.com');
+    console.log("went to website");
     await browser.close();
+    console.log("closed");
 })();
